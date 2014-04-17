4e24a24c0ec3d0a567bca1343d60b281191d77b9
megazear7 <alexlockhart7@gmail.com>
2014-04-16 20:19:35 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alexandmikayla::Application.config.secret_key_base = '9d23fcd26d68bd8c7a967d0f748e3e42aeca43b0a56d80eb2c48932616eab9dce407958b2d1c5db08e281c757a0fcb16d02b99b3b184af56c7842886e984b04b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Alexandmikayla::Application.config.session_store :cookie_store, key: '_alexandmikayla_session'
